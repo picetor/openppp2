a028fde2f86cbaade5fea6b1f45826c3cff6f4cf
Developer <dev@openppp2.local>
2026-06-29 01:52:11 +0800

    cleanup

--- push_fix.ps1
+++
@@ -1,5 +0,0 @@
-Set-ExecutionPolicy -Scope Process -ExecutionPolicy Bypass -Force 2>$null
-cd D:\github\openppp2
-git add -A
-git commit -m "fix: add missing P2PDefs.h and VirtualEthernetTcpMss.h"
-git push origin master
